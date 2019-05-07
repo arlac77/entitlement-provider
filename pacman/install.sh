9d57dedbdef81466f5157864b7ea371381d2b0cd
Markus Felten <Markus.Felten@gmx.de>
2019-05-07 17:29:52 +0200

    chore: pacman / systemd

---
+++ pacman/install.sh
@@ -0,0 +1,34 @@
+
+pre_install() {
+	useradd -U -l -M -r -s /usr/bin/nologin -d /var/lib/{{name}} -c "{{description}}" {{name}}
+}
+
+post_install() {
+	systemctl daemon-reload
+	systemctl enable {{name}}
+	systemctl enable {{name}}.socket
+	systemctl start {{name}}.socket
+}
+
+pre_upgrade() {
+	systemctl stop {{name}}.socket
+	systemctl stop {{name}}
+}
+
+post_upgrade() {
+	systemctl daemon-reload
+	systemctl start {{name}}.socket
+}
+
+pre_remove() {
+	systemctl stop {{name}}.socket
+	systemctl disable {{name}}.socket
+	systemctl stop {{name}}
+	systemctl disable {{name}}
+}
+
+post_remove() {
+	systemctl daemon-reload
+	userdel {{name}}
+	groupdel {{name}}
+}
