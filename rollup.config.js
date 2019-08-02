7ca5ec24b67c52a6a55a4ee9dbba09e1d4d6bbf5
Markus Felten <Markus.Felten@gmx.de>
2019-08-02 15:01:32 +0200

    fix(pacman): introduce socket config

--- rollup.config.js
+++ rollup.config.js
@@ -6,50 +6,11 @@ import json from "rollup-plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import pkg from "./package.json";
 
-// require('module').builtinModules
 const external = [
-  "assert",
-  "async_hooks",
-  "buffer",
-  "child_process",
-  "cluster",
-  "console",
-  "constants",
-  "crypto",
-  "dgram",
-  "dns",
-  "domain",
-  "events",
-  "fs",
-  "http",
-  "http2",
-  "https",
-  "inspector",
-  "module",
-  "net",
-  "os",
-  "path",
-  "perf_hooks",
-  "process",
-  "punycode",
-  "querystring",
-  "readline",
-  "repl",
-  "stream",
-  "string_decoder",
-  "sys",
-  "timers",
-  "tls",
-  "trace_events",
-  "tty",
-  "url",
-  "util",
-  "v8",
-  "vm",
-  "zlib",
+  ...builtins,
 
-
-  "koa"
+  "koa",
+  "sd-daemon"
 ];
 
 export default Object.keys(pkg.bin || {}).map(name => {
@@ -60,13 +21,13 @@ export default Object.keys(pkg.bin || {}).map(name => {
       format: "cjs",
       banner:
         '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
-      interop: false
+      interop: false,
+      externalLiveBindings: false
     },
     plugins: [
       commonjs(),
       resolve(),
       json({
-      //  include: "package.json",
         preferConst: true,
         compact: true
       }),
