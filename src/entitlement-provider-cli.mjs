9786062fe9c1eb14948d2f2d914f4f5fd55787e8
Markus Felten <Markus.Felten@db.com>
2019-10-31 10:58:35 +0100

    fix: use @kronos-integration/service-koa

--- src/entitlement-provider-cli.mjs
+++ src/entitlement-provider-cli.mjs
@@ -1,36 +1,26 @@
-import { resolve } from "path";
 import program from "commander";
-import { expand } from "config-expander";
-import { removeSensibleValues } from "remove-sensible-values";
 import { version, description } from "../package.json";
 import { defaultServerConfig, server } from "./server.mjs";
-import { StandaloneServiceManager } from "@kronos-integration/service";
 import ServiceSystemd from "@kronos-integration/service-systemd";
+import ServiceKOA from "@kronos-integration/service-koa";
 
 program
   .version(version)
   .description(description)
-  .option("-c, --config <dir>", "use config directory")
   .action(async () => {
-    const sm = new ServiceSystemd();
-
-    const configDir = process.env.CONFIGURATION_DIRECTORY || program.config;
-
-    const config = await expand(configDir ? "${include('config.json')}" : {}, {
-      constants: {
-        basedir: configDir || process.cwd(),
-        installdir: resolve(__dirname, "..")
-      },
-      default: {
-        version,
-        ...defaultServerConfig
-      }
-    });
-
-    console.log(removeSensibleValues(config));
-
     try {
-      await server(config);
+      const sm = new ServiceSystemd();
+      sm.registerServiceFactory(ServiceKOA);
+      sm.declareService(
+        {
+          name: "http",
+          type: "koa"
+        },
+        true
+      );
+
+      await sm.start();
+      //await server(config);
     } catch (error) {
       console.log(error);
     }
