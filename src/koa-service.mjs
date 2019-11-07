7c09f0531b424f7bc1268222a9a5ce9aed2b1b13
Markus Felten <Markus.Felten@gmx.de>
2019-11-07 22:05:26 +0100

    fix: auth again

--- src/koa-service.mjs
+++ src/koa-service.mjs
@@ -1,4 +1,26 @@
 import Router from "koa-better-router";
+import BodyParser from "koa-bodyparser";
+import { accessTokenGenerator } from "./auth.mjs";
+
+export const config = {
+  jwt: {
+    options: {
+      algorithm: "RS256",
+      expiresIn: "12h"
+    }
+  },
+  ldap: {
+    url: "ldap://ldap.mf.de",
+    bindDN: "uid={{username}},ou=accounts,dc=mf,dc=de",
+    entitlements: {
+      base: "ou=groups,dc=mf,dc=de",
+      attribute: "cn",
+      scope: "sub",
+      filter:
+        "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=mf,dc=de))"
+    }
+  }
+};
 
 export function setupKoaService(koaService) {
   const router = Router({
@@ -12,6 +34,13 @@ export function setupKoaService(koaService) {
     ctx.body = "hello world";
     return next();
   });
+
+  router.addRoute(
+    "POST",
+    "/authenticate",
+    BodyParser(),
+    accessTokenGenerator(config)
+  );
 
   koaService.koa.use(router.middleware());
 }
--- src/server.mjs
+++ src/server.mjs
@@ -1,29 +1,5 @@
-import Koa from "koa";
-import BodyParser from "koa-bodyparser";
-import Router from "koa-better-router";
-import { accessTokenGenerator } from "./auth.mjs";
-
 const Provider = require("oidc-provider");
 
-export const defaultServerConfig = {
-    jwt: {
-      options: {
-        algorithm: "RS256",
-        expiresIn: "12h"
-      }
-    },
-    ldap: {
-      url: "ldap://ldap.mf.de",
-      bindDN: "uid={{username}},ou=accounts,dc=mf,dc=de",
-      entitlements: {
-        base: "ou=groups,dc=mf,dc=de",
-        attribute: "cn",
-        scope: "sub",
-        filter:
-          "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=mf,dc=de))"
-      }
-    }
-  };
   
 export async function server(config) {
   const configuration = {
@@ -50,21 +26,6 @@ export async function server(config) {
 
   const provider = new Provider('https://mfelten.dynv6.net/services/entitlement-provider', configuration);
   
-  const app = new Koa();
-
-  const router = Router({
-    notFound: async (ctx, next) => {
-      console.log("route not found", ctx.request.url);
-      return next();
-    }
-  });
-
-  router.addRoute(
-    "POST",
-    "/authenticate",
-    BodyParser(),
-    accessTokenGenerator(config)
-  );
 
   console.log(provider.app);
   //app.use(provider.app);
