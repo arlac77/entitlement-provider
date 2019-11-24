0ddadc7dd2a49dc51e380219f941ab9704cbbad3
Markus Felten <Markus.Felten@gmx.de>
2019-11-24 20:57:03 +0100

    fix(package): @kronos-integration/service@7.1.2

--- src/entitlement-provider.mjs
+++ src/entitlement-provider.mjs
@@ -20,10 +20,10 @@ export async function setup(sp) {
     http: {
       type: ServiceKOA,
       endpoints: {
+        "/state": { ...GET, connected: "service(health).state" },
         "/state/uptime": { ...GET, connected: "service(health).uptime" },
         "/state/cpu": { ...GET, connected: "service(health).cpu" },
         "/state/memory": { ...GET, connected: "service(health).memory" },
-        "/state": { ...GET, connected: "service(health).state" },
         "/authenticate": {...POST, connected: "service(auth).access_token" }
       }
     },
@@ -41,18 +41,7 @@ export async function setup(sp) {
     }
   });
 
-  const [koaService, authService, ldapService, healthService] = services;
-
-  /*
-  authService.endpoints.ldap.connected = ldapService.endpoints.authenticate;
-  koaService.endpoints["/state/uptime"].connected =
-    healthService.endpoints.uptime;
-  koaService.endpoints["/state/memory"].connected =
-  healthService.endpoints.memory;
-  koaService.endpoints["/state/cpu"].connected = healthService.endpoints.cpu;
-  koaService.endpoints["/state"].connected = healthService.endpoints.state;
-  koaService.endpoints["/authenticate"].connected = authService.endpoints.access_token;
-*/
+  const [koaService] = services;
 
   koaService.koa.use(endpointRouter(koaService));
 
