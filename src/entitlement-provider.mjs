7e21adca6e9c4079fe970fa4b6cbfc2ae4e540ae
Markus Felten <Markus.Felten@gmx.de>
2019-11-25 14:36:10 +0100

    feat: add service-admin

--- src/entitlement-provider.mjs
+++ src/entitlement-provider.mjs
@@ -3,6 +3,7 @@ import ServiceKOA from "@kronos-integration/service-koa";
 import ServiceHealthCheck from "@kronos-integration/service-health-check";
 import ServiceLDAP from "@kronos-integration/service-ldap";
 import ServiceAuthenticator from "@kronos-integration/service-authentication";
+import ServiceAdmin from "@kronos-integration/service-admin";
 
 import {
   CTXInterceptor,
@@ -25,7 +26,8 @@ export async function setup(sp) {
         "/state/uptime": { ...GET, connected: "service(health).uptime" },
         "/state/cpu": { ...GET, connected: "service(health).cpu" },
         "/state/memory": { ...GET, connected: "service(health).memory" },
-        "/authenticate": {...POST, connected: "service(auth).access_token" }
+        "/authenticate": {...POST, connected: "service(auth).access_token" },
+        "/services": {...GET, connected: "service(admin).services" }
       }
     },
     auth: {
@@ -39,12 +41,11 @@ export async function setup(sp) {
     },
     health: {
       type: ServiceHealthCheck
+    },
+    admin: {
+      type: ServiceAdmin
     }
   });
-
-  const [koaService] = services;
-
-  koaService.koa.use(endpointRouter(koaService));
 
   await sp.start();
   await Promise.all(services.map(s => s.start()));
