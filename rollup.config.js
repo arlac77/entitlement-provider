e336e284944e3d450f661e2e43f5c3227986ed3c
Markus Felten <Markus.Felten@gmx.de>
2019-12-05 16:16:21 +0100

    fix(rollup): switch to @rollup/plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import resolve from "rollup-plugin-node-resolve";
+import resolve from '@rollup/plugin-node-resolve';
+import json from "@rollup/plugin-json";
+
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
 import native from "rollup-plugin-native";
-import json from "@rollup/plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import builtins from "builtin-modules";
 import pkg from "./package.json";
